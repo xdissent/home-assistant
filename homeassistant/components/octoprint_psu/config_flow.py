1f01b2522596e085b8af2e189bd085d458f9c4dc
Greg Thornton <xdissent@me.com>
2020-07-01 22:37:04 -0500

    change connection class

--- homeassistant/components/octoprint_psu/config_flow.py
+++ homeassistant/components/octoprint_psu/config_flow.py
@@ -59,8 +59,7 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Octoprint PSU."""
 
     VERSION = 1
-    # TODO pick one of the available connection classes in homeassistant/config_entries.py
-    CONNECTION_CLASS = config_entries.CONN_CLASS_UNKNOWN
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
