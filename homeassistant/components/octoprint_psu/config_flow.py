7decc72a752e9330ec6ae5f2289befca80acb98e
Greg Thornton <xdissent@me.com>
2020-07-03 00:48:38 -0500

    config flow wip

--- homeassistant/components/octoprint_psu/config_flow.py
+++ homeassistant/components/octoprint_psu/config_flow.py
@@ -1,58 +1,23 @@
 """Config flow for Octoprint PSU integration."""
+import asyncio
 import logging
+from typing import Optional
 
+from octorest import (
+    AuthorizationRequestPollingResult,
+    OctoRest,
+    WorkflowAppKeyRequestResult,
+)
 import voluptuous as vol
 
-from homeassistant import config_entries, core, exceptions
+from homeassistant import config_entries
+from homeassistant.const import CONF_URL, CONF_USERNAME
 
-from .const import DOMAIN  # pylint:disable=unused-import
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-# TODO adjust the data schema to the data that you need
-DATA_SCHEMA = vol.Schema({"host": str, "username": str, "password": str})
-
-
-class PlaceholderHub:
-    """Placeholder class to make tests pass.
-
-    TODO Remove this placeholder class and replace with things from your PyPI package.
-    """
-
-    def __init__(self, host):
-        """Initialize."""
-        self.host = host
-
-    async def authenticate(self, username, password) -> bool:
-        """Test if we can authenticate with the host."""
-        return True
-
-
-async def validate_input(hass: core.HomeAssistant, data):
-    """Validate the user input allows us to connect.
-
-    Data has the keys from DATA_SCHEMA with values provided by the user.
-    """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    hub = PlaceholderHub(data["host"])
-
-    if not await hub.authenticate(data["username"], data["password"]):
-        raise InvalidAuth
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": "Name of the device"}
+DATA_SCHEMA = vol.Schema({CONF_URL: str, CONF_USERNAME: str})
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -61,30 +26,115 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
+    def __init__(self):
+        """Initialize flow."""
+        self._url: Optional[str] = None
+        self._username: Optional[str] = None
+        self._client: Optional[OctoRest] = None
+        self._workflow_url: Optional[str] = None
+        self._workflow_result: Optional[WorkflowAppKeyRequestResult] = None
+        self._api_key: Optional[str] = None
+        self._name: Optional[str] = None
+
+    async def _async_init_workflow(self):
+        """Init the app keys workflow."""
+        self._client = OctoRest(url=self._url)
+        supported = await self.hass.async_add_executor_job(
+            self._client.probe_app_keys_workflow_support
+        )
+        if not supported:
+            return False
+        workflow_url = await self.hass.async_add_executor_job(
+            self._client.start_authorization_process,
+            f"Home Assistant ({self.flow_id})",
+            self._username,
+        )
+        self.hass.async_create_task(self._async_poll_workflow(workflow_url))
+        return True
+
+    async def _async_poll_workflow(self, url, timeout=60):
+        """Poll the app keys workflow."""
+        interval = 1
+        elapsed = 0
+
+        while elapsed < timeout:
+            _LOGGER.debug("Checking workflow url: %s", url)
+            # TODO: Try/catch
+            (polling_result, api_key) = await self.hass.async_add_executor_job(
+                self._client.poll_auth_request_decision, url
+            )
+            if polling_result == AuthorizationRequestPollingResult.NOPE:
+                return await self._async_finish_workflow(
+                    WorkflowAppKeyRequestResult.NOPE
+                )
+            if polling_result == AuthorizationRequestPollingResult.GRANTED:
+                return await self._async_finish_workflow(
+                    WorkflowAppKeyRequestResult.GRANTED, api_key,
+                )
+            _LOGGER.debug("No workflow status yet: %s", url)
+            await asyncio.sleep(interval)
+            elapsed += interval
+        await self._async_finish_workflow(WorkflowAppKeyRequestResult.TIMED_OUT)
+
+    async def _async_finish_workflow(self, result, api_key=None):
+        _LOGGER.debug("Finishing workflow: %s %s", result, api_key)
+        if result == WorkflowAppKeyRequestResult.GRANTED:
+            await self._async_set_api_key(api_key)
+            await self._async_get_name()
+        await self.hass.config_entries.flow.async_configure(
+            self.flow_id, {"result": result},
+        )
+
+    async def _async_set_api_key(self, api_key):
+        self._api_key = api_key
+        await self.hass.async_add_executor_job(self._client.load_api_key, api_key)
+
+    async def _async_get_name(self):
+        settings = await self.hass.async_add_executor_job(self._client.settings)
+        _LOGGER.debug("Got settings: %s", settings)
+        self._name = settings["appearance"]["name"]
+
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
-        errors = {}
-        if user_input is not None:
-            try:
-                info = await validate_input(self.hass, user_input)
+        if user_input is None:
+            return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
 
-                return self.async_create_entry(title=info["title"], data=user_input)
-            except CannotConnect:
-                errors["base"] = "cannot_connect"
-            except InvalidAuth:
-                errors["base"] = "invalid_auth"
-            except Exception:  # pylint: disable=broad-except
-                _LOGGER.exception("Unexpected exception")
-                errors["base"] = "unknown"
+        errors = {}
+        try:
+            self._url = user_input[CONF_URL]
+            self._username = user_input[CONF_USERNAME]
+            workflow = await self._async_init_workflow()
+            _LOGGER.debug("Workflow supported: %s", workflow)
+            if workflow:
+                return await self.async_step_app_keys_workflow()
+            errors["base"] = "invalid_auth"
+        except Exception:  # pylint: disable=broad-except
+            _LOGGER.exception("Unexpected exception")
+            errors["base"] = "unknown"
 
         return self.async_show_form(
             step_id="user", data_schema=DATA_SCHEMA, errors=errors
         )
 
+    async def async_step_app_keys_workflow(self, user_input=None):
+        """Handle the workflow step."""
+        if not user_input:
+            return self.async_external_step(step_id="app_keys_workflow", url=self._url)
+        self._workflow_result = user_input["result"]
+        return self.async_external_step_done(next_step_id="app_keys_workflow_result")
 
-class CannotConnect(exceptions.HomeAssistantError):
-    """Error to indicate we cannot connect."""
+    async def async_step_app_keys_workflow_result(self, user_input=None):
+        """Handle the workflow result step."""
+        _LOGGER.debug("Workflow result step: %s %s", self._api_key, self._name)
+        if self._workflow_result == WorkflowAppKeyRequestResult.GRANTED:
+            return await self.async_step_finish()
+        if self._workflow_result == WorkflowAppKeyRequestResult.NOPE:
+            return self.async_abort(reason="auth_denied")
+        if self._workflow_result == WorkflowAppKeyRequestResult.TIMED_OUT:
+            return self.async_abort(reason="timed_out")
+        return self.async_abort(reason="unknown")
 
-
-class InvalidAuth(exceptions.HomeAssistantError):
-    """Error to indicate there is invalid auth."""
+    async def async_step_finish(self, user_input=None):
+        """Handle the final step."""
+        _LOGGER.debug("FINISH STEP: %s %s", self._api_key, self._name)
+        return self.async_show_form(step_id="finish", data_schema=DATA_SCHEMA)
